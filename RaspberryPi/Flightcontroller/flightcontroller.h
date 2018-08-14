9144d0214350ae8a60dce9d9a75bdcc60e71972e
Sirkii <bigsplitt@gmail.com>
2018-08-14 15:37:49 +0200

    fix

---
+++ RaspberryPi/Flightcontroller/flightcontroller.h
@@ -0,0 +1,24 @@
+#ifndef FLIGHCONTROLLER_H
+#define FLIGHTCONTROLLER_H 1
+
+
+
+class Flightcontroller{
+public:
+
+    /*
+     *
+     */
+    Flightcontroller();
+
+    /*
+     * destructor
+     */
+    ~Flightcontroller();
+
+
+
+
+};
+
+#endif /* Flightcontroller_H */
