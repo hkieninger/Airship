6a005f0b78ff9325e1d397774fb929a7558b5be8
hans <hans1.kieninger@web.de>
2018-08-18 20:12:29 +0200

    fixed bug in motor class, which would have damaged the esc

--- RaspberryPi/motor/motor.cpp
+++ RaspberryPi/motor/motor.cpp
@@ -2,53 +2,89 @@
 #include <stdlib.h>
 #include <pigpio.h>
 #include <unistd.h>
+#include <stdexcept>
 
 #include "motor.h"
 
+//TODO: reduce TIMEs to minimum and find optimal values for PWs (pulse width)
+#define ZERO_PW 1000
+#define MAX_PW (1900 - ZERO_PW)
+#define STARTUP_PW 1600
+#define STARTUP_THRESHOLD_PW 1100
+#define ZERO_TIME (1000 * 1000)
+#define STARTUP_TIME (300 * 1000)
+#define RELAIS_TIME (6 * 1000) //from datasheet
+#define ARM_TIME (500 * 1000)
+
 Motor::Motor(int signalpin, int relaypin){
-  //sets relaypin to output
   this->relaypin = relaypin;
   this->signalpin = signalpin;
   gpioSetMode(relaypin, PI_OUTPUT);
-  //sets signalpin to pwm
-  gpioServo(signalpin, 0);
+  gpioWrite(relaispin, 0);
+  usleep(RELAIS_TIME);
+  powerOn();
+  lastThrust = 0;
 }
 
 Motor::~Motor() {
+  powerOff();
+}
+
+static inline void thrust2pw(int thrust) {
+  return ZERO_PW + abs(thrust) * MAX_PW / MAX_THRUST;
+}
+
+static inline void setZero() {
+  gpioServo(signalpin, ZERO_PW); //turn motor off
+  usleep(ZERO_TIME); //wait until it halts
+  lastThrust = 0;
+}
+
+static inline void setESC(int pw) {
+  //to start the motor you need more power
+  if(pw < STATUP_PW && thrust2pw(lastThrust) < STARTUP_THRESHOLD_PW) {
+    gpioServo(signalpin, STARTUP_PW);
+    usleep(STARTUP_TIME);
+  }
+  gpioServo(signalpin, pw);
+}
+
+void Motor::setThrust(int thrust){
+  if(thrust > MAX_THRUST || thrust < -MAX_THRUST)
+    throw std::invalid_argument("set motor thrust: thrust is out of bounds");
+
+  if(thrust > 0) {
+    if(lastThrust < 0) {
+      setZero();
+      gpioWrite(relaispin, 0); //turn off the relais
+      usleep(RELAIS_TIME); //wait until the relais switches position
+    }
+    setESC(thrust2pw(thrust));
+  } else if(thrust < 0) {
+    if(lastThrust >= 0) {
+      if(lastThrust > 0)
+        setZero();
+      gpioWrite(relaispin, 1); //turn the relais on
+      usleep(RELAIS_TIME); //wait until the relais switches position
+    }
+    setESC(thrust2pw(thrust));
+  } else { //thrust == 0
+    setZero();
+    gpioWrite(relaispin, 0); //turn off the relais
+  }
+  lastThrust = thrust;
+}
+
+void Motor::powerOn(){
   gpioServo(signalpin, 0);
-
+  usleep(ARM_TIME);
 }
 
-
-void Motor::setSpeed(int speed){
-  if (speed < 0){
-    gpioWrite(relaypin, 1);
-  }else{
-    gpioWrite(relaypin, 0);
-  }
-
-  if(running){
-    gpioServo(signalpin, 1000 + (speed * 10));
-  }else{
-    startop();
-    gpioServo(signalpin, 1000 + (speed * 10));
-  }
-
-  if(abs(speed) < 10){
-    running = 0;
-  }else{
-    running = 1;
-  }
-  //write the speed to the esc
-
-}
-
-void Motor::arm(){
+void Motor::powerOff() {
+  setThrust(0);
   gpioServo(signalpin, 0);
-  usleep(1000*500);
 }
 
-void startup(){
-  gpioServo(signalpin, 1600);
-  usleep(300*1000);
+void Motor::getThrust() {
+  return lastThrust;
 }
--- RaspberryPi/servo/servo.cpp
+++ RaspberryPi/servo/servo.cpp
@@ -1,12 +1,11 @@
 #include <pigpio.h>
+#include <stdexcept>
 
 #include "servo.h"
 
 Servo::Servo(int signalpin){
-  //sets relaypin to output
   this->signalpin = signalpin;
-  //sets signalpin to pwm
-  gpioServo(signalpin, 0);
+  setAngle(0);
 }
 
 Servo::~Servo() {
@@ -15,10 +14,11 @@ Servo::~Servo() {
 
 
 void Servo::setAngle(int angle){
-  gpioServo(signalpin, 500 + (angle + 90) * 2000/180);
-
+  if(angle > 90 || angle < -90)
+    throw std::invalid_argument("servo set angle: angle is out of bounds");
+  gpioServo(signalpin, 500 + (angle + 90) * 2000 / 180);
 }
 
-void Servo::poweroff(){
+void Servo::powerOff(){
   gpioServo(signalpin, 0);
 }
