adf80fbb83517abb587f7c31f937ed11afae301e
Sirkii <bigsplitt@gmail.com>
2018-09-11 10:18:40 +0200

    flightcontroller

--- RaspberryPi/Flightcontroller/flightcontroller.cpp
+++ RaspberryPi/Flightcontroller/flightcontroller.cpp
@@ -20,12 +20,43 @@ Flightcontroller::forward(int speed){
   motorrightspeed = speed;
   motorleft.setSpeed(speed);
   motorright.setSpeed(speed);
+  servotop.setAngle(0);
+  servoleft.setAngle(0);
+  servoright.setAngle(0);
 }
 
-Flightcontroller::left(int level){
+Flightcontroller::turn(int speedLeftmotor, int speedRightmotor, int dir){
+  //upmost pedal
+  int absat180 = abs(dir - 180);
+  if(absat180 <= 90){
+    servotop.setAngle(absat180);
+  }else{
+    servotop.setAngle(180-absat180);
+  }
+  //right pedal rotates to whole math 120 degrees
+  dir = dir - 120
+  if(dir<0){
+    dir = dir + 360
+  }
+  absat180 = abs(dir - 180);
+  if(absat180 <= 90){
+    servoright.setAngle(absat180);
+  }else{
+    servoright.setAngle(180-absat180);
+  }
 
-}
+  //left pedal rotates for another 120 degrees
+  dir = dir - 120
+  if(dir<0){
+    dir = dir + 360
+  }
+  absat180 = abs(dir - 180);
+  if(absat180 <= 90){
+    servoleft.setAngle(absat180);
+  }else{
+    servoleft.setAngle(180-absat180);
+  }
 
-Flightcontroller::right(int level){
+
 
 }
